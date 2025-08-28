5a1e48e38f2251d3b29aeaa345be820534c43e35
Gerard Tabone <gerardtabone@yahoo.com>
2025-08-28 17:28:48 +0200

    verbose ast

--- lib/langoneext/run.ml
+++ lib/langoneext/run.ml
@@ -5,8 +5,8 @@ let get_ast (s: string): Ast.expr =
   Parser.prog Lexer.read lexbuf
 
 (* Get AST from string and print AST *)
-(* let get_ast_verbose (s: string): Ast.expr = 
+let get_ast_verbose (s: string): Ast.expr = 
   let lexbuf = Lexing.from_string s in 
   let ast = Parser.prog Lexer.read lexbuf in
   let () = Printf.printf "Parsed AST:\n%s\n" (Ast.string_of_expr ast) in
-    ast *)
+    ast
